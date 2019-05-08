c2de98e6069fc8733239ef0e876b8ea88c735036
tks358 <tks35881@gmail.com>
2019-05-08 16:20:04 +0800

    0502 lesson (route page & alertdialog)

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -13,12 +13,14 @@ class BMI extends StatelessWidget{
         primarySwatch: Colors.blue,
       ),
       //home: StatefulWidgetPage(),
-      home: Tester()
+      //home: Tester()
+      home: TesterPage()
     );
   }
 
 }
 class Tester extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
     //return Text('啊啊啊啊啊啊啊');
@@ -33,13 +35,118 @@ class Tester extends StatelessWidget{
           children: <Widget>[
             Text('a'),
             Text('b'),
-            Text('c')
+            Text('c'),
+
           ],
         ),
       )
 
         //body: Text('fefefefe'),
     );
+  }
+
+}
+class _Tester extends State<TesterPage>{
+  var _value = false;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tester'),
+      ),
+    body: Container(
+      child: Column(
+        children: <Widget>[
+          Switch(
+              onChanged: (bool value){
+               setState((){
+                 _value = value;
+               });
+              },
+                value:_value
+          ),
+        Radio(
+          groupValue: _value,
+          //onChanged: _onChange,
+          value: 0,
+        ),
+          RaisedButton(
+            child:Text('aaa'),
+            onPressed: (){
+              showDialog(
+                  context: context,
+                  builder:(_){
+                    return AlertDialog(
+                      title: Text('aaa'),
+                      content: Text('content'),
+                      actions: <Widget>[
+                        FlatButton(
+                          child:Text('cancel'),
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+
+                        )
+                      ],
+                    );
+//                    return Center(
+//                      child: CircularProgressIndicator(),
+//                    );
+                  }
+              );
+            },
+          ),
+          RaisedButton(
+            child:Text('new page'),
+            onPressed: (){
+              Navigator.of(context).push(PageRoute());
+            }
+          )
+
+
+
+        ],
+      ),
+    )
+    );
+  }
+  _onChange(int a){
+
+  }
+}
+
+class PageRoute extends MaterialPageRoute{
+      PageRoute() : super(builder:(contenxt){
+          return NewPage();
+      });
+}
+
+class NewPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _NewPageState();
+  }
+
+}
+
+class _NewPageState extends State<NewPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('page 2')
+      ),
+    );
+  }
+}
+
+
+class TesterPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Tester();
   }
 
 }
